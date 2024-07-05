fc9e5067933b78f13614378b4d537542b4d9546f
James <james.al.littlefield@gmail.com>
2024-07-05 12:02:34 -0500

    Delete Packages directory

--- Packages/aegis.lua
+++
@@ -1,9 +0,0 @@
-local REQUIRED_MODULE = require(script.Parent._Index["lumin_aegis@0.2.0"]["aegis"])
-export type Key = REQUIRED_MODULE.Key 
-export type State = REQUIRED_MODULE.State 
-export type Spring = REQUIRED_MODULE.Spring 
-export type Compute = REQUIRED_MODULE.Compute 
-export type SpringInfo = REQUIRED_MODULE.SpringInfo 
-export type Animatable = REQUIRED_MODULE.Animatable 
-export type DefaultKeys = REQUIRED_MODULE.DefaultKeys 
-return REQUIRED_MODULE
